90186348fe9c1a2e63d27f5ef5e76047b2debbf9
Diana Sh <86669473+Nevskayad@users.noreply.github.com>
2022-10-20 15:10:33 +0300

    Add inputs to signIn page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,70 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
+import visibility from '../assets/svg/visibility.svg';
+
 function SignIn() {
-  return <h1>Sign In</h1>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const { email, password } = formData;
+
+  const navigate = useNavigate();
+
+  const onChange = () => {};
+
+  return (
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">З поверненням!</p>
+        </header>
+
+        <main>
+          <form>
+            <input
+              type="email"
+              className="emailInput"
+              placeholder="Email"
+              value={email}
+              onChange={onChange}
+            />
+
+            <div className="passwordInputDiv">
+              <input
+                type={showPassword ? 'text' : 'password'}
+                className="passwordInput"
+                placeholder="Пароль"
+                id="password"
+                value={password}
+                onChange={onChange}
+              />
+
+              <img
+                src={visibility}
+                alt="показати пароль"
+                className="showPassword"
+                onClick={() => setShowPassword((prevState) => !prevState)}
+              />
+            </div>
+
+            <Link to="/forgot-password" className="forgotPasswordLink">
+              Забули пароль?
+            </Link>
+
+            <div className="signInBar">
+              <p className="signInText">Увійти</p>
+              <button className="signInButton">
+                <ArrowRightIcon fill="#ffdab9" width="34px" height="34px" />
+              </button>
+            </div>
+          </form>
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default SignIn;
