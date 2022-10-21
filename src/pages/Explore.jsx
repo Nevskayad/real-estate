a98ecd8bcbb25f5531f4c3c880a67d1ad666e4ec
Diana Sh <86669473+Nevskayad@users.noreply.github.com>
2022-10-21 12:33:37 +0300

    Start working on the explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,5 +1,29 @@
+import { Link } from 'react-router-dom';
+import rentImage from '../assets/jpg/img1.jpg';
+import sellImage from '../assets/jpg/img2.jpg';
+
 function Explore() {
-  return <h1>Explore</h1>;
+  return (
+    <div className="explore">
+      <header>
+        <p className="pageHeader">Пошук</p>
+      </header>
+
+      <main>
+        <p className="exploreCategoryHeading">Категорії</p>
+        <div className="exploreCategories">
+          <Link to="/category/rent">
+            <img src={rentImage} alt="оренда" className="exploreCategoryImg" />
+            <p className="exploreCategoryName">Оренда квартир та домівок</p>
+          </Link>
+          <Link to="/category/sell">
+            <img src={sellImage} alt="продаж" className="exploreCategoryImg" />
+            <p className="exploreCategoryName">Продаж нерухомого майна</p>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Explore;
