f0ab871248c613f0d52d9f38e0d6f1974deb576f
Diana Sh <86669473+Nevskayad@users.noreply.github.com>
2022-10-22 13:22:40 +0300

    implement listing details

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -29,7 +29,73 @@ function Listing() {
     fetchListing();
   }, [navigate, params.listingId]);
 
-  return <div>Listing</div>;
+  if (loading) {
+    return <h3 className="loading">Загрузка...</h3>;
+  }
+
+  return (
+    <main>
+      {/*Slider */}
+
+      <div
+        className="shareIconDiv"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <img src={shareIcon} alt="copy" />
+      </div>
+
+      {shareLinkCopied && <p className="linkCopied">Посилання скопійовано</p>}
+
+      <div className="listingDetails">
+        <p className="listingName">
+          {listing.name},{' '}
+          {listing.offer
+            ? listing.discountedPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
+            : listing.regularPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ' ')}
+          ₴
+        </p>
+        <div className="listingLocation">{listing.location}</div>
+        <p className="listingType">
+          {listing.type === 'rent' ? 'Оренда' : 'Продаж'}
+        </p>
+        {listing.offer && (
+          <p className="discountPrice">
+            {listing.regularPrice - listing.discountedPrice}₴ знижка
+          </p>
+        )}
+
+        <ul className="listingDetailsList">
+          <li>{`Спальні кімнати: ${listing.bedrooms}`}</li>
+          <li>{`Ванні кімнати: ${listing.bathrooms}`}</li>
+          <li>{listing.parking && 'Є місце для парковки'}</li>
+          <li>{listing.furnished && 'З меблями'}</li>
+        </ul>
+
+        <p className="listingLocationTitle">Місцезнаходження</p>
+
+        {/* map */}
+
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link
+            to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`}
+            className="primaryButton"
+          >
+            Зв'язатися з {listing.type === 'rent' ? 'орендодавцем' : 'провацем'}
+          </Link>
+        )}
+      </div>
+    </main>
+  );
 }
 
 export default Listing;
