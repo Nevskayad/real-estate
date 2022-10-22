ac869082bda80fed2fc1d55cfee154cd19d2719b
Diana Sh <86669473+Nevskayad@users.noreply.github.com>
2022-10-22 19:40:06 +0300

    Update EditListing.jsx

--- src/pages/EditListing.jsx
+++ src/pages/EditListing.jsx
@@ -1,5 +1,504 @@
+import { useState, useEffect, useRef } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from 'firebase/storage';
+import { doc, updateDoc, getDoc, serverTimestamp } from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { useNavigate, useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { v4 as uuidv4 } from 'uuid';
+
 function EditListing() {
-  return <div>EditListing</div>;
+  const [geolocationEnabled, setGeolocationEnabled] = useState(true);
+
+  const [loading, setLoading] = useState(false);
+  const [listing, setListing] = useState(false);
+  const [formData, setFormData] = useState({
+    type: 'rent',
+    name: '',
+    bedrooms: 1,
+    bathrooms: 1,
+    parking: false,
+    furnished: false,
+    address: '',
+    offer: false,
+    regularPrice: 0,
+    discountedPrice: 0,
+    totalArea: 0,
+    images: {},
+    latitude: 0,
+    longitude: 0,
+  });
+
+  // Деструктуризація formData
+  const {
+    type,
+    name,
+    bedrooms,
+    bathrooms,
+    parking,
+    furnished,
+    address,
+    offer,
+    regularPrice,
+    discountedPrice,
+    totalArea,
+    images,
+    latitude,
+    longitude,
+  } = formData;
+
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const params = useParams();
+  const isMounted = useRef(true);
+
+  //Перенаправлення якщо оголошення не належить користувачу
+  useEffect(() => {
+    if (listing && listing.userRef !== auth.currentUser.uid) {
+      toast.error('Ви не можете редагувати оголошення');
+      navigate('/');
+    }
+  });
+
+  // Редагування оголошення
+  useEffect(() => {
+    setLoading(true);
+    const fetchListing = async () => {
+      const docRef = doc(db, 'listings', params.listingId);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setListing(docSnap.data());
+        setFormData({ ...docSnap.data(), address: docSnap.data().location });
+        setLoading(false);
+      } else {
+        navigate('/');
+        toast.error('Оголошення не існує');
+      }
+    };
+
+    fetchListing();
+  }, [params.listingId, navigate]);
+
+  // Встановлює userRef для користувача що ввійшов в систему
+  useEffect(() => {
+    if (isMounted) {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setFormData({ ...formData, userRef: user.uid });
+        } else {
+          navigate('/sign-in');
+        }
+      });
+    }
+
+    return () => {
+      isMounted.current = false;
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isMounted]);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    if (discountedPrice >= regularPrice) {
+      setLoading(false);
+      toast.error('Ціна зі знижкою має буте меншою за звичайну ціну');
+      return;
+    }
+
+    if (images.length > 6) {
+      setLoading(false);
+      toast.error('Максимальна кількість зображень - 6');
+      return;
+    }
+
+    let geolocation = {};
+    let location;
+
+    if (geolocationEnabled) {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_REAL_ESTATE_GEOCODE_API_KEY}`
+      );
+
+      const data = await response.json();
+
+      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+      geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
+
+      console.log(data);
+      location =
+        data.status === 'ZERO_RESULTS'
+          ? undefined
+          : data.results[0]?.formatted_address;
+
+      if (location === undefined || location.includes('undefined')) {
+        setLoading(false);
+        toast.error('Будь ласка, введіть коректну адресу');
+        return;
+      }
+    } else {
+      geolocation.lat = latitude;
+      geolocation.lng = longitude;
+    }
+
+    //Зберігання зображень в Firebase
+    const storeImage = async (image) => {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage();
+        const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+
+        const storageRef = ref(storage, 'images/' + fileName);
+
+        const uploadTask = uploadBytesResumable(storageRef, image);
+
+        uploadTask.on(
+          'state_changed',
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            // eslint-disable-next-line default-case
+            switch (snapshot.state) {
+              case 'paused':
+                console.log('Upload is paused');
+                break;
+              case 'running':
+                console.log('Upload is running');
+                break;
+            }
+          },
+          (error) => {
+            // Обробка невдалих завантажень
+            reject(error);
+          },
+          () => {
+            // Обробка успішних завантажень після завершення
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+            });
+          }
+        );
+      });
+    };
+
+    const imageUrls = await Promise.all(
+      [...images].map((image) => storeImage(image))
+    ).catch(() => {
+      setLoading(false);
+      toast.error('Не вдалося загрузити зображення');
+      return;
+    });
+
+    const formDataCopy = {
+      ...formData,
+      imageUrls,
+      geolocation,
+      timestamp: serverTimestamp(),
+    };
+
+    formDataCopy.location = address;
+    delete formDataCopy.images;
+    delete formDataCopy.address;
+    !formDataCopy.offer && delete formDataCopy.discountedPrice;
+
+    //Обновити оголошення
+    const docRef = doc(db, 'listings', params.listingId);
+    await updateDoc(docRef, formDataCopy);
+    setLoading(false);
+    toast.success('Оголошення збережено');
+    navigate(`/category/${formDataCopy.type}/${docRef.id}`);
+  };
+
+  const onMutate = (e) => {
+    let boolean = null;
+
+    if (e.target.value === 'true') {
+      boolean = true;
+    }
+    if (e.target.value === 'false') {
+      boolean = false;
+    }
+
+    //Файли
+    if (e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        images: e.target.files,
+      }));
+    }
+
+    //Текст/Булінові значення/Числа
+    if (!e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        [e.target.id]: boolean ?? e.target.value,
+      }));
+    }
+  };
+
+  if (loading) {
+    return <h3 className="loading">Загрузка...</h3>;
+  }
+
+  return (
+    <div className="profile">
+      <header>
+        <p className="pageHeader">Редагувати оголошення</p>
+      </header>
+
+      <main>
+        <form onSubmit={onSubmit}>
+          <label className="formLabel">Продаж / Оренда</label>
+          <div className="formButtons">
+            <button
+              type="button"
+              className={type === 'sale' ? 'formButtonActive' : 'formButton'}
+              id="type"
+              value="sale"
+              onClick={onMutate}
+            >
+              Продаж
+            </button>
+            <button
+              type="button"
+              className={type === 'rent' ? 'formButtonActive' : 'formButton'}
+              id="type"
+              value="rent"
+              onClick={onMutate}
+            >
+              Оренда
+            </button>
+          </div>
+
+          <label className="formLabel">Назва</label>
+          <input
+            type="text"
+            className="formInputName"
+            id="name"
+            value={name}
+            onChange={onMutate}
+            maxLength="32"
+            minLength="10"
+            required
+          />
+
+          <div className="formRooms">
+            <div>
+              <label className="formLabel">Спальні кімнати</label>
+              <input
+                type="number"
+                className="formInputSmall"
+                id="bathrooms"
+                value={bathrooms}
+                onChange={onMutate}
+                min="1"
+                max="50"
+                required
+              />
+            </div>
+            <div>
+              <label className="formLabel">Ванні кімнати</label>
+              <input
+                type="number"
+                className="formInputSmall"
+                id="bedrooms"
+                value={bedrooms}
+                onChange={onMutate}
+                min="1"
+                max="50"
+                required
+              />
+            </div>
+          </div>
+
+          <label className="formLabel">Наявність місця для паркінгу</label>
+          <div className="formButtons">
+            <button
+              className={parking ? 'formButtonActive' : 'formButton'}
+              type="button"
+              id="parking"
+              value={true}
+              onClick={onMutate}
+              min="1"
+              max="50"
+            >
+              Так
+            </button>
+            <button
+              className={
+                !parking && parking !== null ? 'formButtonActive' : 'formButton'
+              }
+              type="button"
+              id="parking"
+              value={false}
+              onClick={onMutate}
+            >
+              Ні
+            </button>
+          </div>
+
+          <label className="formLabel">Наявність меблів</label>
+          <div className="formButtons">
+            <button
+              className={furnished ? 'formButtonActive' : 'formButton'}
+              type="button"
+              id="furnished"
+              value={true}
+              onClick={onMutate}
+            >
+              Так
+            </button>
+            <button
+              className={
+                !furnished && furnished !== null
+                  ? 'formButtonActive'
+                  : 'formButton'
+              }
+              type="button"
+              id="furnished"
+              value={false}
+              onClick={onMutate}
+            >
+              Ні
+            </button>
+          </div>
+
+          <label className="formLabel">Адреса</label>
+          <textarea
+            id="address"
+            className="formInputAddress"
+            type="text"
+            value={address}
+            onChange={onMutate}
+            required
+          ></textarea>
+
+          {!geolocationEnabled && (
+            <div className="formLatLng flex">
+              <div>
+                <label className="formLabel">Широта</label>
+                <input
+                  type="number"
+                  className="formInputSmall"
+                  id="latitude"
+                  value={latitude}
+                  onChange={onMutate}
+                  required
+                />
+              </div>
+              <div>
+                <label className="formLabel">Довгота</label>
+                <input
+                  type="number"
+                  className="formInputSmall"
+                  id="longitude"
+                  value={longitude}
+                  onChange={onMutate}
+                  required
+                />
+              </div>
+            </div>
+          )}
+
+          <label className="formLabel">Наявність знижки</label>
+          <div className="formButtons">
+            <button
+              className={offer ? 'formButtonActive' : 'formButton'}
+              type="button"
+              id="offer"
+              value={true}
+              onClick={onMutate}
+            >
+              Так
+            </button>
+            <button
+              className={
+                !offer && offer !== null ? 'formButtonActive' : 'formButton'
+              }
+              type="button"
+              id="offer"
+              value={false}
+              onClick={onMutate}
+            >
+              Ні
+            </button>
+          </div>
+
+          <label className="formLabel">Звичайна Ціна</label>
+          <div className="formPriceDiv">
+            <input
+              type="number"
+              className="formInputSmall"
+              id="regularPrice"
+              value={regularPrice}
+              onChange={onMutate}
+              min="300"
+              max="80000000"
+              required
+            />
+            {type === 'rent' && <p className="formPriceText">₴ / місяць</p>}
+          </div>
+
+          {offer && (
+            <>
+              <label className="formLabel">Вигідна ціна</label>
+              <input
+                type="number"
+                className="formInputSmall"
+                id="discountedPrice"
+                value={discountedPrice}
+                onChange={onMutate}
+                min="300"
+                max="80000000"
+                required
+              />
+            </>
+          )}
+
+          <label className="formLabel">Загальна площа</label>
+          <div className="formPriceDiv">
+            <input
+              type="number"
+              className="formInputSmall"
+              id="totalArea"
+              value={totalArea}
+              onChange={onMutate}
+              min="5"
+              max="5000"
+              required
+            />
+            <p className="formPriceText">м2</p>
+          </div>
+
+          <label className="formLabel">Зображення</label>
+          <p className="imagesInfo">
+            Перше зображення слугуватиме обкладинкою (макс. кількість 6).
+          </p>
+          <input
+            type="file"
+            className="formInputFile"
+            id="images"
+            onChange={onMutate}
+            max="6"
+            accept=".jpg,.png,.jpeg"
+            multiple
+            required
+          />
+          <button type="submit" className="primaryButton createListingButton">
+            Редагувати оголошення
+          </button>
+        </form>
+      </main>
+    </div>
+  );
 }
 
 export default EditListing;
