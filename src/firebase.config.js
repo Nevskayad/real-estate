43dcd8031c9c656187419b22d9406aadfc1820b0
Diana Sh <86669473+Nevskayad@users.noreply.github.com>
2023-02-13 13:24:19 +0200

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyA0F207nlMxCCR8xMUz-ENYapTtrvmRvT0',
+  apiKey: 'process.env.REACT_APP_REAL_ESTATE_GEOCODE_API_KEY',
   authDomain: 'real-estate-app-4d00a.firebaseapp.com',
   projectId: 'real-estate-app-4d00a',
   storageBucket: 'real-estate-app-4d00a.appspot.com',
   messagingSenderId: '569586698232',
-  appId: '1:569586698232:web:c477710372a9ffc50eb80d',
+  appId: 'process.env.REACT_APP_REAL_ESTATE_APP_ID',
 };
 
 // Initialize Firebase
